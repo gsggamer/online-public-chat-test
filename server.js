6b15c65b5e5cc4db2733143696e5391862d753a5
Gabriel Guimarães <gabriel.souza.guimaraes01@gmail.com>
2023-07-14 15:22:17 +0000

    Second

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
@@ -18,6 +18,33 @@ io.on('connection', (socket) => {
  socket.on('disconnect', () => {
   console.log('Um cliente se desconectou');
  });
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+ console.log(`Servidor escutando na porta ${port}`);
+});
+*/
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Um cliente se conectou');
+
+  socket.on('chat message', (message) => {
+   io.emit('chat message', message);
+  });
+
+  socket.on('disconnect', () => {
+   console.log('Um cliente se desconectou');
+  });
 });
 
 const port = process.env.PORT || 3000;
