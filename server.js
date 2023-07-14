af0a832fdb599d488d0e87d210725ad83cc63157
Gabriel Guimarães <gabriel.souza.guimaraes01@gmail.com>
2023-07-14 15:01:22 +0000

    Codigobase

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+ console.log('Um cliente se conectou');
+
+ socket.on('chat message', (message) => {
+  io.emit('chat message', message);
+ });
+
+ socket.on('disconnect', () => {
+  console.log('Um cliente se desconectou');
+ });
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+ console.log(`Servidor escutando na porta ${port}`);
+});
